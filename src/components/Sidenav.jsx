31ca379911990aa239d7df8b15f3f4ee1c9abecd
KingLyrics <ekom338.com@gmail.com>
2023-01-13 23:42:23 +0100

    Changes made to Sidnav component.

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,7 +1,34 @@
-import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineMenu,AiOutlineHome } from "react-icons/ai";
+import { useState } from "react";
 
 const Sidenav = () => {
-  return <div></div>;
+  const [nav, setnav] = useState(false);
+
+  const handleNav = ()=>{
+    setnav(!nav);
+  }
+  return (
+    <div>
+      <AiOutlineMenu onClick={handleNav} className="absolute top-4 right-4 z-[99] md:hidden" />
+      {
+        nav ? (
+            <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-[20]">
+                <a href="#main">
+                    <AiOutlineHome size={20}/>
+                    <span className="pl-4">Home</span>
+                </a>
+
+            </div>
+        ):
+        (
+            <div>
+
+            </div>
+
+        )
+      }
+    </div>
+  );
 };
 
 export default Sidenav;
